6af85f40c078328388db22747669894fee605749
hsinlingchen <hchen3141@sdsu.edu>
2022-09-06 19:44:26 -0700

    update

--- hw1.py
+++ hw1.py
@@ -1,21 +1,21 @@
 import sys
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 import plotly.express as px
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 
 
 def main():
-    #Load iris data into DataFrame using pandas
+    # Load iris data into DataFrame using pandas
     iris_names = ["sepal length", "sepal width", "petal length", "petal width", "class"]
     iris = pd.read_csv("https://teaching.mrsharky.com/data/iris.data", names=iris_names)
 
-
-    #simple summary statistics
+    # simple summary statistics
     stat = iris.iloc[:, 0:4]
     stat_mean = stat.mean()
     print("Mean:\n{}".format(stat_mean))
@@ -26,39 +26,62 @@ def main():
     q_sl_25 = np.quantile(stat["sepal length"], 0.25)
     q_sl_50 = np.quantile(stat["sepal length"], 0.5)
     q_sl_75 = np.quantile(stat["sepal length"], 0.75)
-    print("Quartiles of Sepal Length:\n0.25 = {}\n0.50 = {}\n0.75 = {}".format(q_sl_25, q_sl_50, q_sl_75))
+    print(
+        "Quartiles of Sepal Length:\n0.25 = {}\n0.50 = {}\n0.75 = {}".format(
+            q_sl_25, q_sl_50, q_sl_75
+        )
+    )
     q_sw_25 = np.quantile(stat["sepal width"], 0.25)
     q_sw_50 = np.quantile(stat["sepal width"], 0.5)
     q_sw_75 = np.quantile(stat["sepal width"], 0.75)
-    print("Quartiles of Sepal Width:\n0.25 = {}\n0.50 = {}\n0.75 = {}".format(q_sw_25, q_sw_50, q_sw_75))
+    print(
+        "Quartiles of Sepal Width:\n0.25 = {}\n0.50 = {}\n0.75 = {}".format(
+            q_sw_25, q_sw_50, q_sw_75
+        )
+    )
     q_pl_25 = np.quantile(stat["petal length"], 0.25)
     q_pl_50 = np.quantile(stat["petal length"], 0.5)
     q_pl_75 = np.quantile(stat["petal length"], 0.75)
-    print("Quartiles of Petal Length:\n0.25 = {}\n0.50 = {}\n0.75 = {}".format(q_pl_25, q_pl_50, q_pl_75))
+    print(
+        "Quartiles of Petal Length:\n0.25 = {}\n0.50 = {}\n0.75 = {}".format(
+            q_pl_25, q_pl_50, q_pl_75
+        )
+    )
     q_pw_25 = np.quantile(stat["petal width"], 0.25)
     q_pw_50 = np.quantile(stat["petal width"], 0.5)
     q_pw_75 = np.quantile(stat["petal width"], 0.75)
-    print("Quartiles of Petal Width:\n0.25 = {}\n0.50 = {}\n0.75 = {}".format(q_pw_25, q_pw_50, q_pw_75))
+    print(
+        "Quartiles of Petal Width:\n0.25 = {}\n0.50 = {}\n0.75 = {}".format(
+            q_pw_25, q_pw_50, q_pw_75
+        )
+    )
 
-    #plots
-    #scatter plots for sepal length and width
+    # plots
+    # scatter plots for sepal length and width
     sepal_scatter = px.scatter(iris, x="sepal length", y="sepal width", color="class")
-    sepal_scatter.write_html(file="sepal_scatter.html", include_plotlyjs="cdn", auto_open=True)
-    #scatter plots for petal length and width
+    sepal_scatter.write_html(
+        file="sepal_scatter.html", include_plotlyjs="cdn", auto_open=True
+    )
+    # scatter plots for petal length and width
     petal_scatter = px.scatter(iris, x="petal length", y="petal width", color="class")
-    petal_scatter.write_html(file="petal_scatter.html", include_plotlyjs="cdn", auto_open=True)
-    #violin plot for sepal length
+    petal_scatter.write_html(
+        file="petal_scatter.html", include_plotlyjs="cdn", auto_open=True
+    )
+    # violin plot for sepal length
     sl_violin = px.violin(iris, y="sepal length", color="class")
-    sl_violin.write_html(file="sepal_length_violin.html", include_plotlyjs="cdn", auto_open=True)
-    #violin plot for petal length
+    sl_violin.write_html(
+        file="sepal_length_violin.html", include_plotlyjs="cdn", auto_open=True
+    )
+    # violin plot for petal length
     sw_violin = px.violin(iris, y="sepal width", color="class")
-    sw_violin.write_html(file="sepal_width_violin.html", include_plotlyjs="cdn", auto_open=True)
-    #histogram for sepal length
+    sw_violin.write_html(
+        file="sepal_width_violin.html", include_plotlyjs="cdn", auto_open=True
+    )
+    # histogram for sepal length
     hist_fig = px.histogram(iris, x="sepal length", y="sepal width", color="class")
     hist_fig.write_html(file="sepal_hist.html", include_plotlyjs="cdn", auto_open=True)
 
-
-    #Analyze and build models
+    # Analyze and build models
     # DataFrame to numpy values
     X_orig = iris.iloc[:, 0:4].values
     y = iris["class"].values
